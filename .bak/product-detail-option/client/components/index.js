e6d71cdfeb9ff0e4af09a49b65de9c499e7c3a52
mikeumus <mikeumus@gmail.com>
2017-11-15 01:50:57 +0000

    master-custom-diff0 remove .bak folder

--- .bak/product-detail-option/client/components/index.js
+++
@@ -1,9 +0,0 @@
-export { default as ProductTags } from "./tags";
-export { default as PriceRange } from "./priceRange";
-export { default as VariantList } from "./variantList";
-export { default as ProductMetadata } from "./metadata";
-export { default as ProductField } from "./productField";
-export { default as ChildVariant } from "./childVariant";
-export { default as ProductDetail } from "./productDetail";
-export { default as AddToCartButton } from "./addToCartButton";
-export { default as ProductNotFound } from "./productNotFound";
--- .bak/product-detail-option/client/containers/index.js
+++
@@ -1,4 +0,0 @@
-export { default as SocialContainer } from "./socialContainer";
-export { default as PublishContainer } from "./publishContainer";
-export { default as VariantListContainer } from "./variantListContainer";
-export { default as ProductOptionContainer } from "./productDetailContainer";
